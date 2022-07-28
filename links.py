a81746b44de68d31534c4886644fd9dc317079a9
João Paulo Magalhães <109976267+jpmm4@users.noreply.github.com>
2022-07-28 18:17:20 +0100

    Delete links.py

--- links.py
+++
@@ -1,2 +0,0 @@
-Print("link for pycharm: https://code-with-me.global.jetbrains.com/T9t4LJGuKTlmKt-WSlpmzg#p=PC&fp=60DD7CB4678C1D53E0B7157DBFF7020C27A11BA8F35FD6319447FB5F118CC9EC")
-Print("IP: 192.168.1.101")
